b61184b773805bdc2b26b08031393307ef116cfd
nathgarr <nathgarr@outlook.fr>
2021-09-09 09:16:27 +0200

    fix:modal

--- app.jS/aap.js
+++ app.jS/aap.js
@@ -1,1 +1,6 @@
-var myModal = new bootstrap.Modal(document.getElementById('myModal'), options)
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
