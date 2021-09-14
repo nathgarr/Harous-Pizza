0958b323e44ad2f44dbca740bad82702ff8bf994
nathgarr <nathgarr@outlook.fr>
2021-09-14 11:55:05 +0200

    feat:offcnva and accordion

--- app.jS/aap.js
+++ app.jS/aap.js
@@ -4,3 +4,7 @@ var myInput = document.getElementById('myInput')
 myModal.addEventListener('shown.bs.modal', function () {
   myInput.focus()
 })
+var offcanvasElementList = [].slice.call(document.querySelectorAll('.offcanvas'))
+var offcanvasList = offcanvasElementList.map(function (offcanvasEl) {
+  return new bootstrap.Offcanvas(offcanvasEl)
+})
